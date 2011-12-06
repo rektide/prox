0c3ff1fc204626895078252eb3385abb8ddb0575
rektide <rektide@voodoowarez.com>
2011-12-06 22:47:15 +0000

    defaultChains implementation, feed obj into ctx.

--- prox.js
+++ prox.js
@@ -1,104 +1,107 @@
 var cc= require("cc")
 
-function handlerMaker(obj) {
-	// build a chain runner
-	function runner(default)
-	{
-		var chain= cc(arguments)
-		return function(){
-			var result = chain.execute({args:arguments,undefinedOnlyNonReturn:true})
-			return result
-		}
+
+function completerFilter(ctx)
+	{return ctx ? ctx.result : undefined}
+
+// build a chain runner
+function runner(obj,filter)
+{
+	filter.filter= completerFilter
+	var chain= cc(filter,{undefinedOnlyNonReturnable:true})
+	return function(){
+		var result= chain.execute(obj,{args:arguments,obj:obj})
+		if(result)
+			return result.result
 	}
-	function completer(ctx)
-	{
-		if(ctx.result)
-			return ctx.result
-		return undefined
-	}
-	
-	var prox = {
-	}
+}
 
-	return function prox(obj) {
-		obj= obj||{}
-		this.prox= prox
-
-		function transparentGetOwnPropertyDescriptor(ctx) {
-			var name= args[0],
-			  desc = Object.getOwnPropertyDescriptor(obj, name)
-			// a trapping proxy's properties must always be configurable
-			if (desc !== undefined) { desc.configurable = true; }
-			ctx.result= desc
-			return false
-		}
-		this.getOwnPropertyDescriptor= runner([transparentGetOwnPropertyDescriptor,completer])
-		
-		function transparentGetPropertyDescriptor(ctx) {
-			var name= args[0],
-			  desc = Object.getPropertyDescriptor(obj, name) // not in ES5
-			// a trapping proxy's properties must always be configurable
-			if (desc !== undefined) { desc.configurable = true; }
-			ctx.result= desc
-			return desc
-		}
-		this.getPropertyDescriptor= runner([transparentGetPropertyDescriptor,completer])
-
-		function transparentGetOwnPropertyNames() {
-			return Object.getOwnPropertyNames(obj);
-		}
-		this.getOwnPropertyNames= runner([transparentGetOwnPropertyNames,completer])
-
-
-		getPropertyNames: function() {
-			return Object.getPropertyNames(obj);                // not in ES5
-		},
-		defineProperty: function(name, desc) {
-			Object.defineProperty(obj, name, desc);
-		},
-		delete:     function(name) { 
-			return delete obj[name]; 
-		},   
-		fix:        function() {
-			if (Object.isFrozen(obj)) {
-			var result = {};
-			Object.getOwnPropertyNames(obj).forEach(function(name) {
-				result[name] = Object.getOwnPropertyDescriptor(obj, name);
-			});
-			return result;
+var defaultChains= {
+	getOwnPropertyDescriptor: function(ctx) {var name= ctx.args[0],
+		  desc= Object.getOwnPropertyDescriptor(ctx.obj, name)
+		// a trapping proxy's properties must always be configurable
+		if(desc !== undefined) {desc.configurable= true}
+		ctx.result= desc
+	},
+	getPropertyDescriptor: function(ctx) {var name= ctx.args[0],
+		  desc= Object.getPropertyDescriptor(ctx.obj, name) // not in ES5
+		// a trapping proxy's properties must always be configurable
+		if(desc !== undefined) {desc.configurable= true}
+		ctx.result= desc
+	},
+	getOwnPropertyNames: function(ctx) {
+		ctx.result= Object.getOwnPropertyNames(ctx.obj)
+	},
+	getPropertyNames: function(ctx) {
+		ctx.result= Object.getPropertyNames(ctx.obj)                // not in ES5
+	},
+	defineProperty: function(ctx) {var name = ctx.args[0], desc = ctx.args[1]
+		Object.defineProperty(ctx.obj, name, desc)
+	},
+	delete: function(ctx) {var name= ctx.args[0]
+		ctx.result= delete ctx.obj[name]
+	},   
+	fix: function(ctx) {
+		if (Object.isFrozen(ctx.obj)) {
+			var result= {}
+			Object.getOwnPropertyNames(ctx.obj).forEach(function(name) {
+				result[name]= Object.getOwnPropertyDescriptor(ctx.obj, name);
+			})
+			ctx.result= result
 		}
 		// As long as obj is not frozen, the proxy won't allow itself to be fixed
-			return undefined; // will cause a TypeError to be thrown
-		},
-		has:        function(name) { return name in obj; },
-		hasOwn:     function(name) { return ({}).hasOwnProperty.call(obj, name); },
-		get:        function(receiver, name) { return obj[name]; },
-		set:        function(receiver, name, val) { obj[name] = val; return true; }, // bad behavior when set fails in non-strict mode
-		enumerate:  function() {
-			var result = [];
-			for (var name in obj) { result.push(name); };
-				return result;
-		},
-		keys: function() { return Object.keys(obj); }
-	}();
+		// will cause a TypeError to be thrown
+	},
+	has: function(name) {var name= ctx.args[0]
+		ctx.result= name in ctx.obj},
+	hasOwn: function(name) {var name= ctx.args[0]
+		ctx.result= ({}).hasOwnProperty.call(ctx.obj, name)},
+	get: function(ctx) {var receiver= ctx.args[0], name= ctx.args[1]
+		ctx.result= obj[name]
+	},
+	set: function(ctx) {var receiver= ctx.args[0], name= ctx.args[1], val= ctx.args[2]
+		ctx.obj[name]= val
+		ctx.result= true}, // bad behavior when set fails in non-strict mode
+	enumerate: function(ctx) {
+		var result= []
+		for (var name in ctx.obj)
+			result.push(name)
+		ctx.result= result
+	},
+	keys: function(ctx) {
+		ctx.result= Object.keys(obj);
+	}
 }
-//var proxy = Proxy.create(handlerMaker(obj));
+
+function handlerMaker(obj,args) {
+	return new function prox(obj) {
+		this._obj= obj
+		var chains= this._chains= {}
+		for(var i in defaultChains) {
+			var chain= defaultChains[i]
+			chains[i]= chain
+			this[i]= runner(obj,chain)
+		}
+		return this
+	}(obj||{});
+}
+//var proxy= Proxy.create(handlerMaker(obj));
 
 function doOrMake(a) {
-	var handler = handleMaker(a||{})
-	var proxied = Proxy.create(handler)
-	proxied._prox = handler.prox // TODO: plugin that creates this exposure
+	var handler= handleMaker(a||{})
+	var proxied= Proxy.create(handler)
+	proxied._prox= handler.prox // TODO: plugin that creates this exposure
 	return proxied
 }
 
 
 /*
 module prox {
-	export var mkprox = doOrMake(handleMaker); }
+	export var mkprox= doOrMake(handleMaker); }
 */
 
 /*
-exports.mkprox = doOrMake(handleMaker);
+exports.mkprox= doOrMake(handleMaker);
 */
 
 
