ac18c7057b5919749f40b9f076a015d292be5e8d
rektide <rektide@voodoowarez.com>
2011-12-11 20:52:41 -0500

    Add crud plugin to expose basic object CRUD event source.

---
+++ plugin/crud.js
@@ -0,0 +1,55 @@
+var eventEmitter= require("events").EventEmitter
+
+if(typeof exports === undefined)
+	exports= {}
+
+var crud= exports.enhance= exports.crud= function(o,opts) {
+	opts= opts||{}
+	var chains= o._chains,
+	  reads= !!opts.reads,
+	  updateOnly= !!opts.updateOnly,
+	  ctxFul= !!opts.ctxFul,
+	  evt= new eventEmitter()
+	if(!chains)
+		throw "No _chains"
+
+	if(!ctxFul) {
+		// does not expose request ctx directly, safer-like.
+		chain.set.chains.push(function(){
+			return !updateOnly ? 
+				function(ctx){var name= ctx.args[1], val= ctx.args[2]
+					evt.emit(o[name]?"update":"create",name,val)
+				}
+			: function(ctx){
+					evt.emit("update",name,val)
+				}
+		}())
+		chain.delete.chain.push(function(ctx){var name= ctx.args[0], dropped= ctx.result
+			evt.emit("delete",name,dropped)
+		})
+		if(reads)
+			chain.get.chain.push(function(ctx){var name= ctx.args[0], returned= ctx.result
+				evt.emit("read",name,returned)
+			})
+	} else {
+		// simpler path that emits the entire ctx for app to dice and mangle in inadvisable ways
+		chain.set.chains.push(function(){
+			return !updateOnly ? 
+				function(ctx){var name= ctx.args[1]
+					evt.emit(o[name]?"update":"create",ctx)
+				}
+			: function(ctx){
+					evt.emit("update",ctx)
+				}
+		}())
+		chain.delete.chain.push(function(ctx){
+			evt.emit("delete",ctx)
+		})
+		if(reads)
+			chain.get.chain.push(function(ctx){
+				evt.emit("read",ctx)
+			})
+
+	}
+	return evt
+}
