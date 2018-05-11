9872a37c1a50d6fc65d41cd7361091592684cf17
rektide de la faye <rektide@voodoowarez.com>
2018-05-10 20:34:31 -0400

    create initial Prox class

--- prox.js
+++ prox.js
@@ -10,7 +10,17 @@ export const pluginsSymbol = new Symbol("plugins")
 /**
 * prox is a proxy 'handler' instance, pointing to a specific obj
 */
-class prox{
+export class Prox{
+	/**
+	* factory method to create a new Prox around an object & return the proxied object
+	*/
+	static make( obj= {}, opts)
+		var p= new Prox( obj, opts)
+		return p.proxied
+	}
+	/**
+	* handler chains are stores as symbols, this maps from the name to the symbol
+	*/
 	static get methodSymbols(){
 		return methods
 	}
@@ -22,25 +32,29 @@ class prox{
 	}
 
 	constructor( obj, opts= {}){
+		const finalize = {
+		  obj,
+		  [plugnsSymbol]: [] // we will initialize this once we create the proxy, via our `plugins`' setter
+		}
 		// define defaults, pull in opts, & save obj
-		const
-		  defaultAndPlugins= [
-		 	...(opts.defaultPlugins|| defaultPlugins),
-			...opts.plugins ],
-		  finalize = {
-			obj,
-			[plugnsSymbol]: []
-		  }
 		Object.assign(this, opts, finalize)
+
 		// create proxyied object that we are the handler for
 		this.proxied= new Proxy( obj, this)
 
 		// set plugins, which will run them
+		const defaultAndPlugins= [
+		  ...(opts.defaultPlugins|| defaultPlugins),
+		  ...opts.plugins ],
 		this.plugins= [...new Set(defaultAndPlugins)]
 	}
 	set plugins( plugins){
-		// uninstall all old plugins
+		// torn about this impl.	ideally every plugin is powered by other state of Prox & uninstall & install
+		// faithfully returns to same state. unsure whether i can make this big demand but going with it for now.
+
+		// uninstall all old plugins, giving us a pristine state
 		this[ pluginsSymbol].forEach( p=> p.uninstall(this))
+		// at this point there really shouldn't be anything on `.chains`
 
 		// install all current plugins
 		this[ pluginsSymbol]= plugins
@@ -50,19 +64,19 @@ class prox{
 		return this[ pluginsSymbol]
 	}
 	free(){
+		// this is probably silly but i'm afraid of things i think
+		// might leak &u want to track these fears here
 		this.proxied= null
 		this.obj= null
 	}
-
 }
-
 
 // for each proxy handler method, create a small `cc` invoker
 Object.keys( defaultChain).forEach( function( method){
 	const chain= new Symbol( method)
 	methods[ method]= chain
 	// create a handler method taking all the trap arguments
-	prox.prototype[ method]= function ( ...args){
+	Prox.prototype[ method]= function ( ...args){
 		const
 		  // get target
 		  target= args[0],
@@ -79,4 +93,4 @@ Object.keys( defaultChain).forEach( function( method){
 	}
 })
 	
-export default prox
+export default Prox
