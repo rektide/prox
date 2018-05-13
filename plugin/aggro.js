b4dabe256a0cdcf3c76baf479e46c2cff590c99f
rektide de la faye <rektide@voodoowarez.com>
2018-05-13 17:15:34 -0400

    Pass plugins during aggro prox creation, perform aggro during install

--- plugin/aggro.js
+++ plugin/aggro.js
@@ -1,25 +1,41 @@
 import Prox from "../prox"
 
 /**
-* Aggro plugin will, whenever an object is assigned, insure that that object is wrapped in a proxy.
+* Aggro plugin will, whenever an object is assigned, insure that that object is wrapped in a prox
+* Additionally, aggro attaches the parent & parentKey to the new prox, that point to the aggro prox & the key where it was set.
 */
 export const aggro= {
 	set: function( ctx){
-		var target= ctx.args[ 0]
-		var targetProx= target._prox
-		if( targetProx&& targetProx.parent=== ctx.prox){
-			// object already has a prox & knows
+		const
+		  target= ctx.args[ 0],
+		  key= ctx.args[1],
+		  prox= ctx.prox,
+		  childProx= target._prox
+		if( childProx&& childProx.parent=== ctx.prox&& childProx.parentKey=== key){
+			// object already has a prox with the correct location information
 			return
 		}
-		target= new Prox( target)
+		const
+		  plugins= ctx.prox.aggroPlugins|| ctx.prox.plugins
+		  newProx= new Prox( target, { plugins}),
 		target._prox.parent= ctx.prox
 		target._prox.parentKey= ctx.args[ 1]
 	},
 	install: function( prox){
 		prox.chain("set").install( aggro.set)
+		for( var key of prox.obj){
+			prox.proxied[ key]= prox.proxied[ key]
+		}
 	},
 	uninstall: function( prox){
 		prox.chain("set").uninstall( aggro.set)
+		for( var key of prox.obj){
+			const proxed= prox.obj[ key]
+			if( !proxed._prox){
+				continue
+			}
+			prox.proxied[ key]= prox.proxied[ key]._prox.obj
+		}
 	}
 }
 aggro.set.phase= "postrun"
