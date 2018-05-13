9b0018d1a51f30a4497b2580c58f87d2c62140a2
rektide de la faye <rektide@voodoowarez.com>
2018-05-13 17:02:17 -0400

    revise aggro plugin, track position.

--- plugin/aggro.js
+++ plugin/aggro.js
@@ -1,15 +1,27 @@
-var prox= require("../prox").prox
+import Prox from "../prox"
 
-if(typeof exports === undefined)
-	exports= {}
-
-function aggro(o,opts) {
-	o._enhance("aggro",function(o) {
-		o._chains.set.chain.push(function(ctx) {
-			var val= ctx.results
-			if(typeof val == "object" && !val._chains)
-				ctx.results= prox(val)
-		})
-	})
+/**
+* Aggro plugin will, whenever an object is assigned, insure that that object is wrapped in a proxy.
+*/
+export const aggro= {
+	set: function( ctx){
+		var target= ctx.args[ 0]
+		var targetProx= target._prox
+		if( targetProx&& targetProx.parent=== ctx.prox){
+			// object already has a prox & knows
+			return
+		}
+		target= new Prox( target)
+		target._prox.parent= ctx.prox
+		target._prox.parentKey= ctx.args[ 1]
+	},
+	install: function( prox){
+		prox.chain("set").install( aggro.set)
+	},
+	uninstall: function( prox){
+		prox.chain("set").uninstall( aggro.set)
+	}
 }
-exports.enhance= exports.aggro= aggro
+aggro.set.phase= "postrun"
+
+export default aggro
