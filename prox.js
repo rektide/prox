c5d6146a59323b245b0900b3035a083d29685fcc
rektide de la faye <rektide@voodoowarez.com>
2018-12-16 21:52:41 -0500

    Extend PhasedMiddleware rather than aggregate it.

--- prox.js
+++ prox.js
@@ -1,16 +1,16 @@
 import PhasedMiddleware from "phased-middleware"
 import { pipelines, pipelineNames} from "./pipeline.js"
-import reflect from "./plugin/reflect.js"
-import propProx from "./plugin/prop-prox.js"
+import defaults from "./defaults.js"
 
 export const basePlugins = [ reflect, propProx]
 
 let _id= 0
 
+
 /**
 * prox is a proxy 'handler' instance, pointing to a specific obj
 */
-export class Prox{
+export class Prox extends PhasedMiddleware{
 	/**
 	* factory method to create a new Prox around an object & return the proxied object
 	*/
@@ -19,8 +19,8 @@ export class Prox{
 		return p.proxied
 	}
 
-	constructor( obj, opts= {}){
-		const plugins= [ ...opts.basePlugins|| basePlugins, ...opts.plugins|| []]
+	constructor( obj, { pipelines, middlewares, name}= defaults){
+		super({ pipelines, middlewares, name})
 		Object.defineProperties( this, {
 			// create proxyied object that we are the handler for
 			proxied: {
@@ -30,55 +30,12 @@ export class Prox{
 			obj: {
 				value: obj
 			},
-			// the middleware runner that executes our proxy handler methods
-			phasedMiddleware: {
-				value: new PhasedMiddleware({
-					pipelines,
-					plugins
-				})
-			},
-			// a unique identifier for this prox instance
-			id: {
-				value: ++_id
-			}
 		})
-		console.log("x", typeof this.proxied)
-	}
-	set plugins( plugins){
-		this.phasedMiddleware.replace( plugins)
-	}
-	get plugins(){
-		return this.phasedMiddleware.middlewares
-	}
-	addPlugin( plugin){
-		this.phasedMiddleware.install( plugin)
-	}
-	exec( method, value){
-		return this.phasedMiddleware.exec[ method]( value)
 	}
 }
 pipelineNames.forEach( method=> {
-	Prox.prototype[ method]= function( value){
-		return this.exec( method, value)
+	Prox.prototype[ method]= function( ...args){
+		return this.exec( method, null, args)
 	}
 })
-
-/**
-* To make debugging easier, handlers are permitted a suffix to their name. Strip this of to their name. Strip this of to their name. Find the suffix-less name.
-*/
-export function stripHandlerSuffix( name){
-	if( chainSymbols[ name]){
-		// name is the exact name, had no suffix
-		return name
-	}
-	for( const chain in chainSymbols){
-		if( name.startsWith( chain)){
-			return chain
-		}
-	}
-	const err= new Error(`Failed to de-suffix \`${name}\` to a chain name`)
-	err.name= name
-	throw err
-}
-
 export default Prox
