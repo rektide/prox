5675ebb8895e20a052d1e53ca5a3daf1f27e2673
rektide de la faye <rektide@voodoowarez.com>
2019-01-06 20:43:20 -0500

    Generate a class name when extending Cursor.

--- cursor.js
+++ cursor.js
@@ -1,18 +1,21 @@
 import { Cursor as PhasedMiddlewareCursor} from "phased-middleware/cursor.js"
 
-export function extend( klass){
-	return class extends klass{
-		get prox(){
-			return this.phasedMiddleware
-		}
-		set prox( prox){
-			this.phasedMiddleware= prox
-		}
-	}
+export function extend( klass= PhasedMiddlewareCursor,{ name}= {}){
+	const
+	  name= name|| "Prox"+ klass.name,
+	  wrapper={[ name]: class extends klass{
+			get prox(){
+				return this.phasedMiddleware
+			}
+			set prox( prox){
+				this.phasedMiddleware= prox
+			}
+	  }} 
+	return wrapper[ name]
 }
 
 export const
-  cursor= extend( PhasedMiddlewareCursor),
+  cursor= extend( PhasedMiddlewareCursor), // PMC's "name" is 'Cursor'
   Cursor= cursor,
-  PhasedMiddlewareCursor= cursor
+  ProxCursor= cursor
 export default cursor
