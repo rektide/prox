73733abf3e6cdbf50713917b4ce80360c7ffff60
rektide de la faye <rektide@voodoowarez.com>
2019-01-20 20:04:51 -0500

    finish aggro 'key' rename in tests.

--- .test/aggro.js
+++ .test/aggro.js
@@ -17,7 +17,7 @@ tape("aggro adds prox to child", function(t){
 	t.notOk( foo._prox, "original foo does not have a _prox")
 	const data= fooProx.pluginData( aggro, foo)
 	t.equal( data.parent, o._prox, "child points to `parent`")
-	t.equal( data.parentKey, "foo", "child knowns it's key via `parentKey`")
+	t.equal( data.key, "foo", "child knowns it's key via `key`")
 	t.end()
 })
 
@@ -30,7 +30,7 @@ tape("grandchildren also proxed by aggro", function(t){
 	t.ok( o.foo.bar._prox, "proxed grandchild has a `_prox`")
 	const grandData= o.foo.bar._prox.pluginData( aggro, grandchild)
 	t.equal( grandData.parent, o.foo._prox, "proxied grandchild points to child prox via `parent`")
-	t.equal( grandData.parentKey, "bar", "proxied grandchild knows it's key via `parentKey`")
+	t.equal( grandData.key, "bar", "proxied grandchild knows it's key via `key`")
 	t.notOk( grandchild._prox, "real grandchild unaffected")
 	t.end()
 })
@@ -60,18 +60,18 @@ tape("one object can be aggroed by two prox", function(t){
 	t.ok( proxed1.alpha._prox, "shared object is proxied accessed via parent")
 	const data1= proxed1.alpha._prox.pluginData( aggro, shared)
 	t.equal( data1.parent, proxed1._prox, "first proxying has parent")
-	t.equal( data1.parentKey, "alpha", "first proxying knows it's parent's key")
+	t.equal( data1.key, "alpha", "first proxying knows it's parent's key")
 
 	proxed2.omega= shared
 	t.notOk( shared._prox, "original shared object being aggroed is unaffected")
 	t.ok( proxed2.omega._prox, "shared object is proxied accessed via parent")
 	const data2= proxed2.omega._prox.pluginData( aggro, shared)
 	t.equal( data2.parent, proxed2._prox, "second proxying has parent")
-	t.equal( data2.parentKey, "omega", "second proxying knows it's parent's key")
+	t.equal( data2.key, "omega", "second proxying knows it's parent's key")
 	// let's re-assert our proxed1, which ought be unchanged
 	const data1b= proxed1.alpha._prox.pluginData( aggro, shared)
 	t.equal( data1b.parent, proxed1._prox, "first proxying still knows it's parent")
-	t.equal( data1b.parentKey, "alpha", "first proxying still knows it's parent's key")
+	t.equal( data1b.key, "alpha", "first proxying still knows it's parent's key")
 	t.end()
 })
 
